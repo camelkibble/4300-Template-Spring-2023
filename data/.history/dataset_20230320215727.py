04d338fdfb8f7f7529e60593c25cb7ca17562ab9
ej <e.j.hangzhou@icloud.com>
2023-03-25 15:51:21 -0400

    add data folder

---
+++ data/.history/dataset_20230320215727.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+business = pd.read_json('yelp_academic_dataset_business.json', lines=True)
+
+#  Filter out businesses that are not restaurants
+
+is_resaurant = business['categories'].str.contains('Restaurants')
+
+# make the is_restaurant column from NA to False
+
+is_resaurant = is_resaurant.fillna(False)
+
+business = business[is_resaurant]
+
+# draw a matplotlib piegram based on the stars
+graph = business['stars'].value_counts().plot(kind='pie', autopct='%1.0f%%', figsize=(6, 6))
---
+++ data/.history/dataset_20230320215729.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+business = pd.read_json('yelp_academic_dataset_business.json', lines=True)
+
+#  Filter out businesses that are not restaurants
+
+is_resaurant = business['categories'].str.contains('Restaurants')
+
+# make the is_restaurant column from NA to False
+
+is_resaurant = is_resaurant.fillna(False)
+
+business = business[is_resaurant]
+
+# draw a matplotlib piegram based on the stars
+graph = business['stars'].value_counts().plot(kind='pie', autopct='%1.0f%%', figsize=(6, 6))
+
+# save the graph to a file
+graph.get_figure().savefig('graph.png')
