1390a02d192701cb50bfd0e08172ea42e5805ce2
ej <e.j.hangzhou@icloud.com>
2023-04-20 15:55:17 -0400

    html

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 # Don't worry about the deployment credentials, those are fixed
 # You can use a different DB name if you want to
 MYSQL_USER = "root"
-MYSQL_USER_PASSWORD = ""
+MYSQL_USER_PASSWORD = "192837465"
 MYSQL_PORT = 3306
 MYSQL_DATABASE = "CityFood"
 
@@ -73,4 +73,4 @@ def recommendations():
     print(name)
     return json.dumps({"restaurant": name})
 
-# app.run(debug=True)
+app.run(debug=True)
